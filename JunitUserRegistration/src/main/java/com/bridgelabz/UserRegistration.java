1d411c016d8e8ac0f61d48511223644a137656cb
balachandar <govindalapudibalachandar@gmail.com>
2021-09-15 18:18:47 +0530

    Added:UserEmail file is added

--- JunitUserRegistration/src/main/java/com/bridgelabz/UserRegistration.java
+++ JunitUserRegistration/src/main/java/com/bridgelabz/UserRegistration.java
@@ -10,9 +10,14 @@ public class UserRegistration {
 		   Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}"); //delcaration of pattern
 		   return pattern.matcher(firstname).matches();
     }
-
+//validate Last name
     public boolean Lastname(String Lastname) {
     	Pattern pattern=Pattern.compile("[A-Z][a-z]{3,}"); //delcaration of pattern
 		   return pattern.matcher(Lastname).matches();
+    }
+    //validate Email id
+    public boolean Email_Id(String Email_Id) {
+    	Pattern pattern=Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{3,})$"); //delcaration of pattern
+		   return pattern.matcher(Email_Id).matches();
     }
 }
--- JunitUserRegistration/src/test/java/com/bridgelabz/TestUserRegistration.java
+++ JunitUserRegistration/src/test/java/com/bridgelabz/TestUserRegistration.java
@@ -13,13 +13,26 @@ public class TestUserRegistration {
     }
 	
 	@Test
-	public  void If_LastNames_Is_AllCases_hould_returnFalse() {
-		boolean  result = registration.firstName("balachandar" );
-		Assert.assertEquals(true,result );
+	public  void If_LastNames_Is_notproper_should_returnfalse() {
+		boolean  result = registration.Lastname("bal");
+		Assert.assertFalse(result); 
 }
 	@Test
 	public  void If_LastNames_Is_Startwith_UpperCase_Should_returnTrue() {
-		boolean  result = registration.firstName("Balachandar" );
+		boolean  result = registration.Lastname("Balachandar");
 		Assert.assertEquals(true,result );
+}
+
+	@Test
+	public  void  If_Email_Id_Isnotproper_should_returnfalse() {
+		UserRegistration registration = new UserRegistration();
+		boolean  result = registration.Email_Id("bal.@gmail.com");
+		Assert.assertFalse(result);
+}
+		@Test
+	public  void If_Email_Id_Should_returnTrue() {
+		UserRegistration registration = new UserRegistration();
+		boolean  result = registration.Email_Id("Bala123@gmail.com");
+		Assert.assertTrue(result );
 }
 }
